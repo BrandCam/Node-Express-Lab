46cf8a5748145e4cb3ad4a2e4acf651c0bc2118f
BrandCam <BrandCam>
2019-01-08 15:21:01 -0600

    post function added and ability to pars JSON objects

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const express = require("express");
 
 const server = express();
 
+//middleware
+server.use(express.json());
+
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(post => {
@@ -51,9 +54,28 @@ server.delete("/api/posts/:id", (req, res) => {
           .json({ message: "The post with the specified ID does not exist." });
       }
     })
-    .catch(err => {
+    .catch(() => {
       res.status(500).json({ error: "The post could not be removed" });
     });
 });
 
+server.post("/api/posts", (req, res) => {
+  const postInfo = req.body;
+  console.log(postInfo);
+  db.insert(postInfo)
+    .then(result => {
+      db.findById(result.id)
+        .then(post => {
+          res.status(201).json(post);
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: "failed to get post by id", error: err });
+        });
+    })
+    .catch(err =>
+      res.status(500).json({ error: "failed to post", error: err })
+    );
+});
 server.listen(3333, () => console.log("server running"));
